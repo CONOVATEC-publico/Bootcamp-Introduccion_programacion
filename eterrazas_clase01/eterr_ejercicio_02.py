4bd5dd7e0b5c56b9707280861024cad37a95b8aa
ETERRAZAS21PE <eterrazas.met.pe21@gmail.com>
2023-04-23 15:38:34 -0500

    Actualizando rama con eterr_ejercicio_02

---
+++ eterrazas_clase01/eterr_ejercicio_02.py
@@ -0,0 +1,9 @@
+"""
+Escribir un programa que pregunte el nombre del usuario en la consola y después devuelva un saludo personalizado.
+"""
+print("Ejercicio 02:")
+print()
+nombre_usuario = input("¿Cuál es su nombre?: ")
+print(f"Recibe un cordial saludo {nombre_usuario}. Tu nombre posee {len(nombre_usuario)} caracteres.")
+print()
+print("Final del script.")
