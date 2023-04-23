bb65e80815a249d9df713c94b0bc8e1e0e056ace
ETERRAZAS21PE <eterrazas.met.pe21@gmail.com>
2023-04-23 12:23:20 -0500

    Enviando contenido de rama clases a la rama eterrazas_clases01

---
+++ eterrazas_clase01/eterr_ejercicio_01.py
@@ -0,0 +1,11 @@
+"""
+Escribir un script que almacene la cadena `Hola Mundo!` en una variable y luego muestre por pantalla
+el contenido de la misma.
+"""
+print("Ejercicio 01:")
+print()
+variable_cadena = "¡Hola Mundo!"
+print(variable_cadena)
+print()
+# print(type(variable_cadena))
+print("El script ha culminado.")
